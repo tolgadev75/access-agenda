637486c9894bc2399e02cf2287a2dccc3b2ab54f
tonytolgadev <erdogantolga6@gmail.com>
2025-05-24 12:31:06 +0200

    optimisation perf

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="dns-prefetch" href="https://nominatim.openstreetmap.org">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
